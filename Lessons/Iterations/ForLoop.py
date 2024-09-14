86bac96d87bf8b7a3a45655f174ec30130c63e4e
danielle rosengren <danielle.rosengren92@gmail.com>
2024-09-14 17:23:59 +0200

    Tested Loops

---
+++ Lessons/Iterations/ForLoop.py
@@ -0,0 +1,20 @@
+array = [2, 1, 3]
+
+# Regular for loop
+# for some_variable in range(len(array)):
+#    print(array[some_variable])
+
+# print("---")
+
+# for each loop
+# for item in array:
+#    print(item)
+
+# print("---")
+
+# for item in array:
+#    print(array[item-1])
+
+for a in range(5):
+   for b in range(5):
+      print(a*b)
---
+++ Lessons/Iterations/WhileLoop.py
@@ -0,0 +1,8 @@
+running = True
+holmfastAge = 18
+counter = 0
+while(running):
+    if (counter == holmfastAge):
+        print(f"Holmfast is {counter} years old")
+        running = False
+    counter += 1
